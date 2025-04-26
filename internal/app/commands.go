9eb815b495777c836884c8c6de27d30825cede7b
gxstxxv <144585373+gxstxxv@users.noreply.github.com>
2025-04-26 14:55:12 +0200

    turned off placeholder info in commandline

--- internal/app/commands.go
+++ internal/app/commands.go
@@ -9,16 +9,16 @@ func (m *Model) handleCommand(value string) tea.Cmd {
 	switch value {
 	case "w":
 		if err := writeFile(m.schmierblatt.Value(), m.files[m.selectedFileIndex]); err != nil {
-			m.commandline.Placeholder = "Error saving Schmierblatt!"
+			// m.commandline.Placeholder = "Error saving Schmierblatt!"
 			logger.Error("Failed to save file", "error", err)
 		} else {
-			m.commandline.Placeholder = "Schmierblatt has been saved!"
+			// m.commandline.Placeholder = "Schmierblatt has been saved!"
 			m.filemenu = initList(m.files, m.selectedFileIndex)
 			logger.Info("File saved successfully", "file", m.files[m.selectedFileIndex])
 		}
 	case "wq":
 		if err := writeFile(m.schmierblatt.Value(), m.files[m.selectedFileIndex]); err != nil {
-			m.commandline.Placeholder = "Error saving Schmierblatt!"
+			// m.commandline.Placeholder = "Error saving Schmierblatt!"
 			logger.Error("Failed to save file", "error", err)
 			return nil
 		}
